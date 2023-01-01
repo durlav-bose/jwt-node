dbb636be7da8d1a58aa6b383d2ed15784e04d4b3
iamdurlav <durlavbose@gmail.com>
2023-01-01 11:16:58 +0600

    laptop commit

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,9 +1,25 @@
+const User = require("../models/User");
+
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+}
+
 module.exports.signup_get = (req,res) => {
   res.render('signup');
 }
 
-module.exports.signup_post = (req,res) => {
-  res.send("signed up");
+module.exports.signup_post = async (req,res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({
+      email: email,
+      password: password
+    })
+    res.status(201).json(user);
+  } catch (error) {
+    handleErrors(error);
+    res.status(400).send("error, user not created...");
+  }
 }
 
 module.exports.login_get = (req,res) => {
@@ -11,5 +27,6 @@ module.exports.login_get = (req,res) => {
 }
 
 module.exports.login_post = (req,res) => {
-  res.send("logged in");
+  const { email, password } = req.body;
+  res.send("logged in ");
 }

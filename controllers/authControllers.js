f6ef2462c65ee57b71bbde9d67e01e995039513a
iamdurlav <durlavbose@gmail.com>
2023-01-01 14:44:02 +0600

    error handling

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,22 @@ const User = require("../models/User");
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
+  let error = {
+    email: "",
+    password: ""
+  }
+
+  if(err.code === 11000) {
+    error.email = "This email is already registered"
+    return error;
+  }
+
+  if(err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties },index) => {
+      error[properties.path] = properties.message
+    })
+  }
+  return error;
 }
 
 module.exports.signup_get = (req,res) => {
@@ -17,8 +33,9 @@ module.exports.signup_post = async (req,res) => {
     })
     res.status(201).json(user);
   } catch (error) {
-    handleErrors(error);
-    res.status(400).send("error, user not created...");
+    const errors = handleErrors(error);
+    console.log('errors :>> ', errors);
+    res.status(400).json({ errors: errors });
   }
 }
 
